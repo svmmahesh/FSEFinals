7887b1fe815634614707eb815e324b6303f396b8
Mahesh <SVM.Mahesh@Chubb.com>
2019-09-29 20:09:57 +0800

    fse

---
+++ ProjectManagerWebAPI/ProjectManager.BusinessLayer/ProjectManagerService.cs
@@ -0,0 +1,210 @@
+﻿using ProjectManager.DataLayer;
+using ProjectManager.Shared.ServiceContracts;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace ProjectManager.BusinessLayer
+{
+    public class ProjectManagerService
+    {
+        public bool AddTask(TaskModel taskModel)
+        {
+            EntityMapper<TaskModel, Task> mapObj = new EntityMapper<TaskModel, Task>();
+            var task = mapObj.Translate(taskModel);
+            return ProjectManagerRepository.InsertTask(task);
+        }
+
+        public bool AddProject(ProjectModel projectModel)
+        {
+            EntityMapper<ProjectModel, Project> mapObj = new EntityMapper<ProjectModel, Project>();
+            var project = mapObj.Translate(projectModel);
+            if(project.Start_Date == default(DateTime))
+            {
+                project.Start_Date = null;
+            }
+            if (project.End_Date == default(DateTime))
+            {
+                project.End_Date = null;
+            }
+            return ProjectManagerRepository.InsertProject(project);
+        }
+
+        public bool AddUser(UserModel userModel)
+        {
+            EntityMapper<UserModel, User> mapObj = new EntityMapper<UserModel, User>();
+            var user = mapObj.Translate(userModel);
+            return ProjectManagerRepository.InsertUser(user);
+        }
+
+        public bool AddParentTask(ParentTaskModel parentTaskModel)
+        {
+            EntityMapper<ParentTaskModel, ParentTask> mapObj = new EntityMapper<ParentTaskModel, ParentTask>();
+            var parentTask = mapObj.Translate(parentTaskModel);
+            return ProjectManagerRepository.InsertParentTask(parentTask);
+        }
+
+        public List<TaskModel> GetTasks()
+        {
+            EntityMapper<Task, TaskModel> mapObj = new EntityMapper<Task, TaskModel>();
+            List<Task> taskList = ProjectManagerRepository.GetTasks();
+            List<TaskModel> taskModels = new List<TaskModel>();
+            foreach (var task in taskList)
+            {
+                var taskModel = mapObj.Translate(task);
+                taskModel.ParentTaskName = ProjectManagerRepository.GetParentTask(task.Parent_ID).ParentTaskName;
+                taskModels.Add(taskModel);                
+            }
+
+            return taskModels;
+        }
+
+        public List<UserModel> GetUsers()
+        {
+            EntityMapper<User, UserModel> mapObj = new EntityMapper<User, UserModel>();
+            List<User> userList = ProjectManagerRepository.GetUsers();
+            List<UserModel> userModels = new List<UserModel>();
+            foreach (var user in userList)
+            {
+                userModels.Add(mapObj.Translate(user));
+            }
+
+            return userModels;
+        }
+
+
+        public List<ProjectModel> GetProjects()
+        {
+            EntityMapper<Project, ProjectModel> mapObj = new EntityMapper<Project, ProjectModel>();
+            List<Project> projectList = ProjectManagerRepository.GetProjects();
+            List<ProjectModel> projectModels = new List<ProjectModel>();
+            foreach (var project in projectList)
+            {
+                var projectModel = mapObj.Translate(project);
+                projectModel.NoOfTasks = ProjectManagerRepository.GetTasks().Where(x => x.Project_ID == projectModel.Project_ID).ToList().Count;
+                projectModel.CompletedTasks = ProjectManagerRepository.GetTasks().Where(x => x.Project_ID == projectModel.Project_ID && x.End_Date.Date <= DateTime.Now.Date).ToList().Count;
+                projectModels.Add(projectModel);
+            }
+
+            return projectModels;
+        }
+
+        public List<ParentTaskModel> GetParentTasks()
+        {
+            EntityMapper<ParentTask, ParentTaskModel> mapObj = new EntityMapper<ParentTask, ParentTaskModel>();
+            List<ParentTask> parentTaskList = ProjectManagerRepository.GetParentTasks();
+            List<ParentTaskModel> parentTaskModels = new List<ParentTaskModel>();
+            foreach (var parent in parentTaskList)
+            {
+                var parentTaskModel = mapObj.Translate(parent);                
+                parentTaskModels.Add(parentTaskModel);
+            }
+
+            return parentTaskModels;
+        }
+
+        public List<UserModel> SearchUsers(string searchKeyWord, string sortBy)
+        {
+            EntityMapper<User, UserModel> mapObj = new EntityMapper<User, UserModel>();
+            List<User> userList = ProjectManagerRepository.SearchUsers(searchKeyWord, sortBy);
+            List<UserModel> userModels = new List<UserModel>();
+            foreach (var user in userList)
+            {
+                userModels.Add(mapObj.Translate(user));
+            }
+
+            return userModels;
+        }
+
+        public List<ProjectModel> SearchProjects(string searchKeyWord, string sortBy)
+        {
+            EntityMapper<Project, ProjectModel> mapObj = new EntityMapper<Project, ProjectModel>();
+            List<Project> projectList = ProjectManagerRepository.SearchProjects(searchKeyWord, sortBy);
+            List<ProjectModel> projectModels = new List<ProjectModel>();
+            foreach (var project in projectList)
+            {
+                projectModels.Add(mapObj.Translate(project));
+            }
+
+            return projectModels;
+        }
+
+        public UserModel GetUser(int userID)
+        {
+            EntityMapper<User, UserModel> mapObj = new EntityMapper<User, UserModel>();
+            User user = ProjectManagerRepository.GetUser(userID);
+            UserModel userModel = new UserModel();
+            userModel = mapObj.Translate(user);
+           
+            return userModel;
+        }
+
+        public ProjectModel GetProject(int projectID)
+        {
+            EntityMapper<Project, ProjectModel> mapObj = new EntityMapper<Project, ProjectModel>();
+            Project project = ProjectManagerRepository.GetProject(projectID);
+            ProjectModel projectModel = new ProjectModel();
+            projectModel = mapObj.Translate(project);
+
+            return projectModel;
+        }
+
+        public TaskModel GetTask(int taskID)
+        {
+            EntityMapper<Task, TaskModel> mapObj = new EntityMapper<Task, TaskModel>();
+            Task task = ProjectManagerRepository.GetTask(taskID);
+            TaskModel taskModel = new TaskModel();
+            taskModel = mapObj.Translate(task);
+
+            return taskModel;
+        }
+
+        public ParentTaskModel GetParentTask(int parentID)
+        {
+            EntityMapper<ParentTask, ParentTaskModel> mapObj = new EntityMapper<ParentTask, ParentTaskModel>();
+            ParentTask parentTask = ProjectManagerRepository.GetParentTask(parentID);
+            ParentTaskModel parentTaskModel = new ParentTaskModel();
+            parentTaskModel = mapObj.Translate(parentTask);
+
+            return parentTaskModel;
+        }
+
+        public bool UpdateUser(UserModel userModel)
+        {
+            EntityMapper<UserModel, User> mapObj = new EntityMapper<UserModel, User>();
+            var user = mapObj.Translate(userModel);
+            return ProjectManagerRepository.UpdateUser(user);
+        }
+
+        public bool UpdateProject(ProjectModel projectModel)
+        {
+            EntityMapper<ProjectModel, Project> mapObj = new EntityMapper<ProjectModel, Project>();
+            var project = mapObj.Translate(projectModel);
+            return ProjectManagerRepository.UpdateProject(project);
+        }
+
+        public bool UpdateTask(TaskModel taskModel)
+        {
+            EntityMapper<TaskModel, Task> mapObj = new EntityMapper<TaskModel, Task>();
+            var task = mapObj.Translate(taskModel);
+            return ProjectManagerRepository.UpdateTask(task);
+        }
+
+        public bool UpdateParentTask(ParentTaskModel parentTaskModel)
+        {
+            EntityMapper<ParentTaskModel, ParentTask> mapObj = new EntityMapper<ParentTaskModel, ParentTask>();
+            var parentTask = mapObj.Translate(parentTaskModel);
+            return ProjectManagerRepository.UpdateParentTask(parentTask);
+        }
+
+        public bool DeleteProject(int projectID)
+        {
+            return ProjectManagerRepository.DeleteProject(projectID);
+        }
+
+        public bool DeleteUser(int userID)
+        {
+            return ProjectManagerRepository.DeleteUser(userID);
+        }        
+    }
+}
---
+++ ProjectManagerWebAPI/ProjectManager.DataLayer.Tests/ProjectManagerRepositoryTests.cs
@@ -0,0 +1,420 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace ProjectManager.DataLayer.Tests
+{
+    public class ProjectManagerRepositoryTests
+    {
+        [Test]
+        public void InsertTaskForValidDataTest()
+        {
+            var validData = new Task()
+            {                       
+                Parent_ID = 1,
+                Project_ID = 1,
+                TaskName = "Generate Scripts",
+                Start_Date = DateTime.Now,
+                End_Date = DateTime.Now.AddDays(1),
+                Priority = 1,
+                Status = true
+            };
+            var isSuccess = ProjectManagerRepository.InsertTask(validData);
+            Assert.AreEqual(true, isSuccess);
+        }
+
+        [Test]
+        public void InsertTaskForInValidDataTest()
+        {
+            var inValidData = new Task()
+            {
+                TaskName = null,
+                Parent_ID = 1,
+                Project_ID = 1,
+                Start_Date = DateTime.Now,
+                End_Date = DateTime.Now.AddDays(1),
+                Priority = 1,
+                Status = true
+            };
+            var isSuccess = ProjectManagerRepository.InsertTask(inValidData);
+            Assert.AreEqual(false, isSuccess);
+        }
+
+        [Test]
+        public void InsertTaskForNullTest()
+        {
+            var isSuccess = ProjectManagerRepository.InsertTask(null);
+            Assert.AreEqual(false, isSuccess);
+        }
+
+        [Test]
+        public void InsertUserForValidDataTest()
+        {
+            var validData = new User()
+            {
+                FirstName = "Reshma",
+                LastName = "Rajendran",
+                Employee_ID = 1,
+                Project_ID = 1,
+                Task_ID = 1
+            };
+            var isSuccess = ProjectManagerRepository.InsertUser(validData);
+            Assert.AreEqual(true, isSuccess);
+        }
+
+        [Test]
+        public void InsertUserForInValidDataTest()
+        {
+            var inValidData = new User()
+            {
+                FirstName = null,
+                LastName = null,
+                Employee_ID = 1,
+                Project_ID = 1,
+                Task_ID = 1
+            };
+            var isSuccess = ProjectManagerRepository.InsertUser(inValidData);
+            Assert.AreEqual(false, isSuccess);
+        }
+
+        [Test]
+        public void InsertUserForNullTest()
+        {
+            var isSuccess = ProjectManagerRepository.InsertUser(null);
+            Assert.AreEqual(false, isSuccess);
+        }
+
+        [Test]
+        public void InsertProjectForValidDataTest()
+        {
+            var validData = new Project()
+            {
+                ProjectName = "Microsoft",
+                Start_Date = null,
+                End_Date = null,
+                Priority = 1
+            };
+            var isSuccess = ProjectManagerRepository.InsertProject(validData);
+            Assert.AreEqual(true, isSuccess);
+        }
+
+        [Test]
+        public void InsertProjectForInValidDataTest()
+        {
+            var inValidData = new Project()
+            {
+                ProjectName = null,
+                Start_Date = null,
+                End_Date = null,
+                Priority = 1
+            };
+            var isSuccess = ProjectManagerRepository.InsertProject(inValidData);
+            Assert.AreEqual(false, isSuccess);
+        }
+
+        [Test]
+        public void InsertProjectForNullTest()
+        {
+            var isSuccess = ProjectManagerRepository.InsertProject(null);
+            Assert.AreEqual(false, isSuccess);
+        }
+
+        [Test]
+        public void InsertParentTaskForValidDataTest()
+        {
+            var validData = new ParentTask()
+            {
+                ParentTaskName = "Build Framework"
+            };
+            var isSuccess = ProjectManagerRepository.InsertParentTask(validData);
+            Assert.AreEqual(true, isSuccess);
+        }
+
+        [Test]
+        public void InsertParentTaskForInValidDataTest()
+        {
+            var inValidData = new ParentTask()
+            {
+                ParentTaskName = null
+            };
+            var isSuccess = ProjectManagerRepository.InsertParentTask(inValidData);
+            Assert.AreEqual(false, isSuccess);
+        }
+
+        [Test]
+        public void InsertParentTaskForNullTest()
+        {
+            var isSuccess = ProjectManagerRepository.InsertParentTask(null);
+            Assert.AreEqual(false, isSuccess);
+        }
+
+        [Test]
+        public void GetTasksTest()
+        {
+            var tasks = ProjectManagerRepository.GetTasks();
+            Assert.NotZero(tasks.Count);
+        }
+
+        [Test]
+        public void GetUsersTest()
+        {
+            var tasks = ProjectManagerRepository.GetUsers();
+            Assert.NotZero(tasks.Count);
+        }
+
+        [Test]
+        public void GetProjectsTest()
+        {
+            var tasks = ProjectManagerRepository.GetProjects();
+            Assert.NotZero(tasks.Count);
+        }
+
+        [Test]
+        public void GetTaskTestForValidDataTest()
+        {
+            var task = ProjectManagerRepository.GetTask(1);
+            Assert.IsNotNull(task);
+        }
+
+        [Test]
+        public void GetTaskTestForInValidDataTest()
+        {
+            var task = ProjectManagerRepository.GetTask(0);
+            Assert.IsNull(task);
+        }
+
+        [Test]
+        public void GetUserTestForValidDataTest()
+        {
+            var task = ProjectManagerRepository.GetUser(1);
+            Assert.IsNotNull(task);
+        }
+
+        [Test]
+        public void GetUserTestForInValidDataTest()
+        {
+            var task = ProjectManagerRepository.GetUser(0);
+            Assert.IsNull(task);
+        }
+
+        [Test]
+        public void GetProjectTestForValidDataTest()
+        {
+            var task = ProjectManagerRepository.GetProject(1);
+            Assert.IsNotNull(task);
+        }
+
+        [Test]
+        public void GetProjectForInValidDataTest()
+        {
+            var task = ProjectManagerRepository.GetProject(0);
+            Assert.IsNull(task);
+        }
+
+        [Test]
+        public void SearchUsersForValidDataTest()
+        {
+            var users = ProjectManagerRepository.SearchUsers("Reshma", "FirstName");
+            Assert.NotZero(users.Count);
+        }
+
+        [Test]
+        public void SearchUsersForInValidDataTest()
+        {
+            var users = ProjectManagerRepository.SearchUsers("Sydney", "FirstName");
+            Assert.Zero(users.Count);
+        }
+
+        [Test]
+        public void SearchUsersForNullTest()
+        {
+            var users = ProjectManagerRepository.SearchUsers(null, null);
+            Assert.Zero(users.Count);
+        }
+
+        [Test]
+        public void SearchProjectsForValidDataTest()
+        {
+            var projects = ProjectManagerRepository.SearchProjects("Microsoft", "StartDate");
+            Assert.NotZero(projects.Count);
+        }
+
+        [Test]
+        public void SearchProjectsForInValidDataTest()
+        {
+            var projects = ProjectManagerRepository.SearchProjects("awsyuwqwnksj", "StartDate");
+            Assert.Zero(projects.Count);
+        }
+
+        [Test]
+        public void SearchProjectsForNullTest()
+        {
+            var projects = ProjectManagerRepository.SearchProjects(null, null);
+            Assert.Zero(projects.Count);
+        }
+
+        [Test]
+        public void UpdateTaskForValidDataTest()
+        {
+            var validData = new Task()
+            {
+                Task_ID = 1,
+                TaskName = "Generate Scripts",
+                Parent_ID = 1,
+                Project_ID = 1,
+                Start_Date = DateTime.Now,
+                End_Date = DateTime.Now.AddDays(1),
+                Priority = 1,
+                Status = true
+            };
+            var isSuccess = ProjectManagerRepository.UpdateTask(validData);
+            Assert.AreEqual(true, isSuccess);
+        }
+
+        [Test]
+        public void UpdateTaskForInValidDataTest()
+        {
+            var inValidData = new Task()
+            {
+                Task_ID = 1,
+                TaskName = null,
+                Parent_ID = 1,
+                Project_ID = 1,
+                Start_Date = DateTime.Now,
+                End_Date = DateTime.Now.AddDays(1),
+                Priority = 1,
+                Status = true
+            };
+            var isSuccess = ProjectManagerRepository.UpdateTask(inValidData);
+            Assert.AreEqual(false, isSuccess);
+        }
+
+        [Test]
+        public void UpdateTaskForNullTest()
+        {
+            var isSuccess = ProjectManagerRepository.UpdateTask(null);
+            Assert.AreEqual(false, isSuccess);
+        }
+
+        [Test]
+        public void UpdateUserForValidDataTest()
+        {
+            var validData = new User()
+            {
+                User_ID = 1,
+                FirstName = "Reshma",
+                LastName = "Rajendran",
+                Employee_ID = 1,
+                Project_ID = 1,
+                Task_ID = 1
+            };
+            var isSuccess = ProjectManagerRepository.UpdateUser(validData);
+            Assert.AreEqual(true, isSuccess);
+        }
+
+        [Test]
+        public void UpdateUserForInValidDataTest()
+        {
+            var inValidData = new User()
+            {
+                User_ID = 1,
+                FirstName = null,
+                LastName = null,
+                Employee_ID = 1,
+                Project_ID = 2,
+                Task_ID = 12
+            };
+            var isSuccess = ProjectManagerRepository.UpdateUser(inValidData);
+            Assert.AreEqual(false, isSuccess);
+        }
+
+        [Test]
+        public void UpdateUserForNullTest()
+        {
+            var isSuccess = ProjectManagerRepository.UpdateUser(null);
+            Assert.AreEqual(false, isSuccess);
+        }
+
+        [Test]
+        public void UpdateProjectForValidDataTest()
+        {
+            var validData = new Project()
+            {
+                Project_ID = 1,
+                ProjectName = "Microsoft",
+                Start_Date = null,
+                End_Date = null,
+                Priority = 1
+            };
+            var isSuccess = ProjectManagerRepository.UpdateProject(validData);
+            Assert.AreEqual(true, isSuccess);
+        }
+
+        [Test]
+        public void UpdateProjectForInValidDataTest()
+        {
+            var inValidData = new Project()
+            {
+                Project_ID = 1,
+                ProjectName = null,
+                Start_Date = null,
+                End_Date = null,
+                Priority = 1
+            };
+            var isSuccess = ProjectManagerRepository.UpdateProject(inValidData);
+            Assert.AreEqual(false, isSuccess);
+        }
+
+        [Test]
+        public void UpdateProjectForNullTest()
+        {
+            var isSuccess = ProjectManagerRepository.UpdateProject(null);
+            Assert.AreEqual(false, isSuccess);
+        }
+
+        [Test]
+        public void UpdateParentTaskForValidDataTest()
+        {
+            var validData = new ParentTask()
+            {
+                Parent_ID = 1,
+                ParentTaskName = "Build Framework"
+            };
+            var isSuccess = ProjectManagerRepository.UpdateParentTask(validData);
+            Assert.AreEqual(true, isSuccess);
+        }
+
+        [Test]
+        public void UpdateParentTaskForInValidDataTest()
+        {
+            var inValidData = new ParentTask()
+            {
+                Parent_ID = 1,
+                ParentTaskName = null
+            };
+            var isSuccess = ProjectManagerRepository.UpdateParentTask(inValidData);
+            Assert.AreEqual(false, isSuccess);
+        }
+
+        [Test]
+        public void UpdateParentTaskForNullTest()
+        {
+            var isSuccess = ProjectManagerRepository.UpdateParentTask(null);
+            Assert.AreEqual(false, isSuccess);
+        }
+
+        [Test]
+        public void DeleteUserTest()
+        {
+            var isSuccess = ProjectManagerRepository.DeleteUser(1);
+            Assert.AreEqual(true, isSuccess);
+        }        
+
+        [Test]
+        public void DeleteProjectTest()
+        {
+            var isSuccess = ProjectManagerRepository.DeleteProject(1);
+            Assert.AreEqual(true, isSuccess);
+        }        
+    }
+}
